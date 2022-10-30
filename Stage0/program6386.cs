074bf6003eec56bc9c40db49c1a578a0fe27ece6
naama2004 <naamaderee@gmail.com>
2022-10-30 23:15:20 +0200

    im alse changing

---
+++ Stage0/program6386.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class program
+    {
+
+    static partial void Welcome6386()
+        {
+            Console.WriteLine("Hi Moriel IM workind");
+        }
+    }
+}
